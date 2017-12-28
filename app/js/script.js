41f4f3d340ee777e6138888b5fe093f9a45f3a81
Quinton Jason <quinton.jason@gmail.com>
2017-12-27 18:44:31 -0600

    logo animation

--- app/js/script.js
+++ app/js/script.js
@@ -70,24 +70,26 @@ if (!$("html").hasClass("lt-ie9")) {
 var logo = document.getElementById("qj-logo");
 var logoQ = document.getElementById("logo-q");
 var logoJ = document.getElementById("logo-j");
+var logoPic = document.getElementById("logo-pic");
 var logoExpanded = true;
 var tl = new TimelineMax({ paused: true });
 
 tl.addLabel("start");
 tl
-	.to(logoJ, 0.5, { x: "-=91" }, "start")
-	.to(logoJ, 0.5, { autoAlpha: 0 }, "start-=.3")
-	.to(logoQ, 0.5, { x: getCenter(logo, logoQ) }, "start");
+	.to(logoJ, 0.45, { x: "-=31" }, "start")
+	.to(logoPic, 0.35, { autoAlpha: 1 }, "start+=.3")
+	.to(logoJ, 0.5, { autoAlpha: 0 }, "start")
+	.to(logoQ, 0.75, { x: getCenter(logo, logoQ) }, "start");
 
 function getCenter(x, y) {
 	// get center of path inside SVG
 	var xWid = x.getBoundingClientRect().width;
 	var yWid = y.getBoundingClientRect().width;
-	return (xWid - yWid) / 4;
+	return (xWid - yWid) / 2;
 }
 
-logo.onclick = function() {
-	logo.classList.toggle("on");
+logo.onclick = function(e) {
+	// logo.classList.toggle("on");
 
 	// slide J from left(-91px) to orig spot and fadeIn if not expanded
 	if (logoExpanded) {
